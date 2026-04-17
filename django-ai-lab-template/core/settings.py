257913d17106b27afc8513a6d5841aae8a396921
Breiner <breinerma16@gmail.com>
2026-04-17 13:55:03 -0500

    Agrego django-browser-reload para que se actualice la vista al navegador automáticamente

--- django-ai-lab-template/core/settings.py
+++ django-ai-lab-template/core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "django_browser_reload",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
